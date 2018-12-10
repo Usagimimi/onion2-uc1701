0504a97e28cd70e3058e52218e83017c128889b2
u.arhangelskiy <Daniil112014>
2018-12-10 13:31:47 +0300

    Clear code and add remarks

--- src/main.c
+++ src/main.c
@@ -13,6 +13,8 @@ int main(int argc, char * argv[])
 	int pattern = 0;
 	int contrast = 5;
 
+	printf("Testing UC1701 driver now.\n");
+
 	if(argc == 2)
 	{
 		sscanf(argv[1], "%d", &pattern);
@@ -23,9 +25,7 @@ int main(int argc, char * argv[])
 		sscanf(argv[1], "%d", &pattern);
 		sscanf(argv[2], "%d", &contrast);
 	}
-
-	printf("Testing UC1701 driver now.\n");
-
+	
 	//init random values generator (needed for demo)
 	srand(time(NULL));
 
@@ -33,8 +33,8 @@ int main(int argc, char * argv[])
 	uc1701_initparams_t params  = {
 		.mosi = 8,	//MOSI
 		.sclk = 7,	//SCLK
-		.cs_h = 6,	//CS
-		.cs_s = 0,	//CS
+		.cs_h = 6,	//CS hardware don't touch!!!
+		.cs_s = 0,	//CS sowftware
 		.led = -1,	//LED backlight (unused)
 		.rst = 3,	//reset
 		.rs = 1,	//D/C
@@ -47,11 +47,11 @@ int main(int argc, char * argv[])
 		return -1;
 	}
 
-	//UC1701_Contrast(4,44);
-	//UC1701_Orientation(scr_normal);
 	UC1701_Contrast(4, 34 + contrast);
+	printf(" Contrast set to %d\n", contrast);
 
 	UC1701_Fill(pattern);
+	printf(" Pattern set to %d\n", pattern);
 
 	UC1701_Flush();
 
